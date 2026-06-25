37d1d0f5216fe222c1f3d5f38b19db3d2aa3d018
luqman-x <181305081+luqman-x@users.noreply.github.com>
2026-06-25 11:42:55 +0000

    Updating Monitor Patient codes

--- 25th_June_2026_Lab-Practice_Code/Monitor.cpp
+++ 25th_June_2026_Lab-Practice_Code/Monitor.cpp
@@ -1,6 +1,6 @@
-
 #include <iostream>
 using namespace std;
+
 class CardiacMonitor{
 private:
     unsigned int normalBeat;
@@ -19,11 +19,54 @@ public:
         totalBeat++;
     }
 
+    void display(){
+        cout << "Total Number of beats : " << totalBeat << endl;
+        cout << "Total Number of Normal beat" << normalBeat << endl;
 
+        unsigned int AbnormalBeat = totalBeat - normalBeat;
+        cout << "Total Number of Abnormal beats: " << AbnormalBeat << endl;
+
+
+        float AbnormalPercentage;
+        if (totalBeat == 0){
+            AbnormalPercentage = 0.0;
+            //return AbnormalPercentage;
+        }
+        else{
+            AbnormalPercentage = (AbnormalBeat * 100)/totalBeat ;
+        }
+        cout << "Total Number of Abnormal Percentages: " << AbnormalPercentage <<"%"<< endl;
+
+        if(AbnormalPercentage >= 20){
+            cout << "Clinical Warning. Readings must be reviewed" << endl;
+        }
+    }
 
     };
 int main(){
+    CardiacMonitor CM;
+    char input;
+    cout << "Enter N for Normal Count" << endl;
+    cout << "Enter A for Abnormal Count" << endl;
+    cout << "Enter E to end and Display" << endl;
 
+    do{
+        cin >> input;
+        if(input == 'N' || input == 'n'){
+            CM.NormalBeatBeatCount();
+        }
+        else if(input == 'A' || input == 'a'){
+            CM.AbnormalBeatCount();
+        }
+        else if( input == 'E' || input == 'e'){
+            CM.display();
+            break;
+        }
+        else{
+            cout << "You have entered the wrong input. Enter only N,A or E"<< endl;
+        }
+
+    }while(true);
 
     return 0;
 }

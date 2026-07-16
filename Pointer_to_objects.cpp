0f992f7a18c17b501ee324ae3f82f8e2b2d42d0f
luqman-x <181305081+luqman-x@users.noreply.github.com>
2026-07-16 13:40:16 +0000

    Initial commit for Pointers to Objects

---
+++ Pointer_to_objects.cpp
@@ -0,0 +1,21 @@
+/*
+Question 1 - Pointer to objects
+Create a class for patients in a hospital. Include data members for the Patient class: name and
+ID numbers.
+● Write member functions to store data members and display them.
+In main():
+1. Create an array of pointers to Patient objects.
+2. Using the new operator, let your user fill this array with as many patients as they want.
+3. Display the details of all the patients in the array using a for loop
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}

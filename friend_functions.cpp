ca9e0be4444e19ddca5dc1ea34bdf4f937899709
luqman-x <181305081+luqman-x@users.noreply.github.com>
2026-07-22 04:02:01 +0000

    Code Completion

--- friend_functions.cpp
+++ friend_functions.cpp
@@ -17,3 +17,72 @@ and prints either “Patient vitals are normal.” or “ALERT: Abnormal vitals 
 */
 
 using namespace std;
+class HeartRate; // Forward decleration
+
+class BloodPressure
+{
+private:
+    int systolic, diastolic;
+
+public:
+    BloodPressure(int s, int d)
+    {
+        systolic = s;
+        diastolic = d;
+    }
+
+    friend bool isHealthy(BloodPressure bp, HeartRate hr);
+};
+
+class HeartRate
+{
+private:
+    int bpm;
+
+public:
+    HeartRate(int BPM)
+    {
+        bpm = BPM;
+    }
+
+    friend bool isHealthy(BloodPressure bp, HeartRate hr);
+};
+
+bool isHealthy(BloodPressure bp, HeartRate hr)
+{
+    return (
+        bp.systolic < 120 &&
+        bp.diastolic < 80 &&
+        hr.bpm >= 60 &&
+        hr.bpm <= 100);
+}
+
+int main()
+{
+    int sys, dia, bpm;
+
+    // Get input from user
+    cout << "Enter systolic pressure: ";
+    cin >> sys;
+
+    cout << "Enter diastolic pressure: ";
+    cin >> dia;
+
+    cout << "Enter heart rate (bpm): ";
+    cin >> bpm;
+
+    // Create objects
+    BloodPressure bp(sys, dia);
+    HeartRate hr(bpm);
+
+    // Check health status
+    if (isHealthy(bp, hr))
+    {
+        cout << "\nPatient vitals are normal." << endl;
+    }
+    else
+    {
+        cout << "\nALERT: Abnormal vitals detected." << endl;
+    }
+    return 0;
+}
